eefffda19e14d5364c4dd4da677ed66c49c23000
bagfaced <114305608+bagfaced@users.noreply.github.com>
2022-09-26 17:32:28 -0400

    Update find.py

--- find.py
+++ find.py
@@ -2,14 +2,14 @@ with open('ids.txt') as f:
     lines=f.readlines()
 f.close()
 
-with open('ids.txt', 'w', encoding="utf-8") as f:
+with open('ids.txt', 'w', encoding="ASCII") as f:
     for x in lines:
         try:
             split=x.split('\t')
             first=split[0]
             hexy=hex(int(first))
             final=bytes.fromhex(hexy[2:])
-            ascii=final.decode("UTF-8")
+            ascii=final.decode("ASCII")
             f.write(ascii)
             f.write('\n')
         except:
